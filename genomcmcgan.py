e94ece7e2edec9db03956400587c258a8a2cf4be
pabswfly <pablo.ianez.picazo@gmail.com>
2020-12-18 10:44:56 +0100

    retreat. we were trying not to import tf globally

--- genomcmcgan.py
+++ genomcmcgan.py
@@ -20,14 +20,6 @@ import numpy as np
 from mcmcgan import MCMCGAN, Discriminator
 from genobuilder import Genobuilder
 
-gpus = tf.config.experimental.list_physical_devices("GPU")
-if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-    except RuntimeError as e:
-        print(e)
-
 
 def run_genomcmcgan(
     genobuilder,
